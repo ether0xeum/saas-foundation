5f3f452c3f1f793e30e47ded68bf95648b691922
Jonny David Lezcano <jonnyda1811@gmail.com>
2025-02-28 14:34:37 -0300

    Added to password to protected django session

--- src/cfehome/views.py
+++ src/cfehome/views.py
@@ -6,10 +6,12 @@ from visits.models import PageVisit
 
 this_dir = pathlib.Path(__file__).resolve().parent
 
+
 def home_view(request, *args, **kwargs):
     if request.user.is_authenticated:
         print(request.user.first_name)
     return about_view(request, *args, **kwargs)
+
 
 def about_view(request, *args, **kwargs):
     qs = PageVisit.objects.all()
@@ -29,11 +31,10 @@ def about_view(request, *args, **kwargs):
     PageVisit.objects.create(path=request.path)
     return render(request, html_template, my_context)
 
+
 def my_old_home_page_view(request, *args, **kwargs):
     my_title = "My Page"
-    my_context = {
-        "page_title": my_title
-    }
+    my_context = {"page_title": my_title}
     html_ = """
     <!DOCTYPE html>
     <html>
@@ -41,7 +42,23 @@ def my_old_home_page_view(request, *args, **kwargs):
             <h1>{page_title} is anything?</h1>
         </body>
     </html>
-    """.format(**my_context)
+    """.format(
+        **my_context
+    )
     # html_file_path = this_dir / "home.html"
     # html_ = html_file_path.read_text()
     return HttpResponse(html_)
+
+VALID_CODE = "abc123"
+
+def pw_protected_view(request, *args, **kwargs):
+    is_allowed = request.session.get('protected_page_allowed') or 0
+    print(request.session.get('protected_page_allowed'), type(request.session.get('protected_page_allowed')))
+    if request.method == "POST":
+        user_pw_sent = request.POST.get("code") or None
+        if user_pw_sent == VALID_CODE:
+            is_allowed = 1
+            request.session['protected_page_allowed'] = is_allowed
+    if is_allowed:
+        return render(request, "protected/view.html", {})
+    return render(request, "protected/entry.html", {})
