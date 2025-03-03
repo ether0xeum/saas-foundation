36a80e5e4ca766ba9b5f84243ed2d3337e7d999b
Jonny David Lezcano <jonnyda1811@gmail.com>
2025-03-02 21:57:39 -0300

    Added user detail page and template permissions

--- src/profiles/views.py
+++ src/profiles/views.py
@@ -7,19 +7,19 @@ User = get_user_model()
 
 @login_required
 def profile_list_view(request):
-    context = {}
+    context = {
+        "object_list": User.objects.filter(is_active=True)
+    }
     return render(request, "profiles/list.html", context)
 
 @login_required
-def profile_view(request, username=None, *rgs, **kwargs):
+def profile_detail_view(request, username=None, *rgs, **kwargs):
     user = request.user
-    print(user.has_perm('auth.view_user'))
-    print(user.get_all_permissions())
-    # profile_user_obj = User.objects.get(username=username)
     profile_user_obj = get_object_or_404(User, username=username)
     is_me = profile_user_obj.id == user.id
-    if is_me:
-        if user.has_perm("visits.view_pagevisit"):
-            pass
-            # qs = PageVisit.object.all()
-    return HttpResponse(f"Hello there {username} - {profile_user_obj.id} - {user.id} - {is_me}")
+    context = {
+        "object": profile_user_obj,
+        "instance": profile_user_obj,
+        "owner": is_me,
+    }
+    return render(request, "profiles/detail.html", context)
